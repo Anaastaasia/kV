67db9c5a9468424e62726f1d62fbb897c8dd5a30
Anastasia <nastazizora@gmail.com>
2024-06-19 23:55:11 +0300

    noterror

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* publick/app.js */
 
 import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import FirstComponent from "./Pages/Main/FirstComponent";
 import MovieDetail from './Pages/Main/MovieDetails'; // Новый компонент для отображения информации о фильме
 import Favorites from './Pages/Main/Favorites';
